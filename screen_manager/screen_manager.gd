5b8a15446630a592619928b4a86d43fe6a37fd69
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-11-13 14:26:50 -0300

    Tela 5 feita

--- screen_manager/screen_manager.gd
+++ screen_manager/screen_manager.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var screens: Array[Screen]
 @export var current_checkpoint: Marker2D
 
-var starting_screen: float = 4
+var starting_screen: float = 5
 
 func _ready() -> void:
 	if starting_screen == null:
