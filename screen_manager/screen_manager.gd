ac43ee492f7b60c9d10b6935b058020bddc7931c
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-11-14 15:28:02 -0300

    Tile set novo

--- screen_manager/screen_manager.gd
+++ screen_manager/screen_manager.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var screens: Array[Screen]
 @export var current_checkpoint: Marker2D
 
-var starting_screen: float = 6
+var starting_screen: float = 0
 
 func _ready() -> void:
 	if starting_screen == null:
