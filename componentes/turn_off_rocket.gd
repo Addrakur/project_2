d5bc7211773bf7b62a1538da10c8337a46d13c85
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-11-18 15:30:10 -0300

    Alterações em obstaculos
    Areas de desligar o foguete e alterar a gravidade ajustadas

--- componentes/turn_off_rocket.gd
+++ componentes/turn_off_rocket.gd
@@ -8,8 +8,8 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.gravity_mult = -1
+		body.rocket_active = false
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
-		body.gravity_mult = 1
+		body.rocket_active = true
