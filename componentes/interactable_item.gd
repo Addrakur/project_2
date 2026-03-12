bbd9f3287258a2051c1ac3788ba8b240e3e3fcb1
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2026-03-12 16:28:52 -0300

    World 2 tela 7 feita

--- componentes/interactable_item.gd
+++ componentes/interactable_item.gd
@@ -2,12 +2,16 @@ class_name InteractableItem
 extends Area2D
 
 @export var manager: DoorManager
+@export var color: Color
 @onready var animation: AnimationPlayer = $animation
+@onready var texture: Sprite2D = $texture
 var interacted: bool = false
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and not interacted:
 		animation.play("interacted")
 		interacted = true
 		manager.check_interactables()
+
+func set_reset_color():
+	texture.modulate = color
