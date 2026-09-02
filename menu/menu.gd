90d7c3f740f998b0ef30f45dca6e38ccc498ccce
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2026-09-02 16:28:46 -0300

    Primeiro mundo pronto para teste

--- menu/menu.gd
+++ menu/menu.gd
@@ -8,3 +8,9 @@ func _on_1_2_pressed() -> void:
 
 func _on_1_3_pressed() -> void:
 	get_tree().change_scene_to_file("uid://kuh8gtrxn3a6")
+
+func _on_1_4_pressed() -> void:
+	get_tree().change_scene_to_file("uid://bprkcayf4m8h4")
+
+func _on_1_5_pressed() -> void:
+	get_tree().change_scene_to_file("uid://62rehkusbvss")
